5e26868a4c045e4a50ab8ad8ccbca2555e7bc93f
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-05-12 14:23:50 +0200

    feat(items): :sparkles: add item constructors and toString

--- src/main/java/edu/estatuas/galleygrub/items/Item.java
+++ src/main/java/edu/estatuas/galleygrub/items/Item.java
@@ -1,4 +1,23 @@
 package edu.estatuas.galleygrub.items;
 
-public class Item {
+public class Item implements Product{
+    String name;
+    Double price;
+    String extra;
+
+    public Item(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Item(String name, Double price, String extra) {
+        this.name = name;
+        this.price = price;
+        this.extra = extra;
+    }
+
+    @Override
+    public String toString(){
+        return extra == null ? (name + "...." + price) : (name + " w/ " + extra + "...." + price);
+    }
 }
