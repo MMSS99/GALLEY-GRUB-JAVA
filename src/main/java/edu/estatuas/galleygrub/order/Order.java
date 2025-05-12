7935bbcd4739c13c1b1e62956d93bd2ef3176109
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-05-12 15:10:45 +0200

    feat(order): :sparkles: itemList method to return ArrayList with order content

--- src/main/java/edu/estatuas/galleygrub/order/Order.java
+++ src/main/java/edu/estatuas/galleygrub/order/Order.java
@@ -3,12 +3,17 @@ package edu.estatuas.galleygrub.order;
 import edu.estatuas.galleygrub.items.Item;
 import edu.estatuas.galleygrub.items.ItemFactory;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order implements Comanda{
     private ArrayList<Item> items = new ArrayList<>();
     private Double total = 0d;
+
+    public ArrayList<Item> itemList(){
+        return items;
+    }
 
     @Override
     public void addItem(String item, double price) {
